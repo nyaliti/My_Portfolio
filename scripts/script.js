0360c156048eeaa8be9f2074143b86b62dca62ba
nyaliti <bnyaliti@gmail.com>
2023-08-16 15:28:20 +0300

    Added project filtering or sorting mechanism

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,25 @@
 // scripts/scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    const filterButtons = document.querySelectorAll('.filter-buttons button');
+    const projects = document.querySelectorAll('.project');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            filterButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+            const filterValue = button.getAttribute('data-filter');
+
+            projects.forEach(project => {
+                if (filterValue === 'all' || project.classList.contains(filterValue)){
+                    project.style.display ='block';
+                    } else {
+                        project.style.display = 'none';
+                    }
+                });
+            });
+    });
+});
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
