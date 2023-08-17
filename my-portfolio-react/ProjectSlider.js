06804cbbbf4a531f192bb67227b98b41bf13245a
nyaliti <bnyaliti@gmail.com>
2023-08-17 17:02:57 +0300

    Added dynamic content with React

---
+++ my-portfolio-react/ProjectSlider.js
@@ -0,0 +1,12 @@
+// src/components/ProjectsSlider.js
+import React from "react";
+
+const ProjectsSlider = () => {
+    return (
+        <div className="project-slider">
+            {/* Add your project carousel or slider here */}
+        </div>
+    );
+};
+
+export default ProjectsSlider;
---
+++ my-portfolio-react/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from "react";
+import './App.css';
+import ProjectsSlider from "./components/ProjectSlider";
+
+
+function App() {
+  return (
+    <div className="App">
+      <ProjectsSlider />
+    </div>
+  );
+}
+
+
+export default App;
