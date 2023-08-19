4cb71b0105206dcad256185f1074893d01f7bd7b
nyaliti <bnyaliti@gmail.com>
2023-08-19 14:00:21 +0300

    Customized navigation bar and added dark mode toggle

---
+++ scripts/dark-mode-toggle.js
@@ -0,0 +1,9 @@
+// scripts/dark-mode-toggle.js
+document.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const body = document.body;
+    
+    darkModeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+    });
+});
