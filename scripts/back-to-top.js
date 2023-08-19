d427ce9c2256f88e4110f539f90c5e9851ac9361
nyaliti <bnyaliti@gmail.com>
2023-08-19 14:40:19 +0300

    Added back to top button

--- scripts/back-to-top.js
+++ scripts/back-to-top.js
@@ -1,19 +1,4 @@
 // scripts/back-to-top.js
-document.addEventListener('DOMContentLoaded', function () {
-    const backToTopButton = document.getElementById('back-to-top');
-    
-    backToTopButton.addEventListener('click', function () {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
-    
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 300) {
-            backToTopButton.style.display = 'block';
-        } else {
-            backToTopButton.style.display = 'none';
-        }
-    });
+document.getElementById('back-to-top').addEventListener('click', function() {
+    window.scrollTo(0, 0);
 });
